e3ba41ffb8f424a2b27279fee094672da6bf3d34
lachlan <lachlan@drawboard.com>
2018-01-17 16:21:02 +1100

    Add project files.

---
+++ UnitTestProject1/UnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using SamplesNative;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var helper = new DeviceLostHelper();
+        }
+    }
+}
